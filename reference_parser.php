ac13df2ed7eefcb8ee56bd628b87a5d4b74ee44d
Roderic Page <rdmpage@gmail.com>
2021-08-14 14:11:00 +0100

    Tidy up parser

--- reference_parser.php
+++ reference_parser.php
@@ -14,44 +14,48 @@ function process_references ($obj)
 		return;
 	}
 	
+	
 	// Call external service to parse reference
 	$n = count($obj->references);
 	for ($i = 0; $i < $n; $i++)
 	{
-		$string = $obj->references[$i]->string;
-		
-		$url = $config['parser_url'] . '/parse?string=' . urlencode($string);
-
-		$json = get($url);
-		if ($json != '')
+		if (isset($obj->references[$i]->string))
 		{
-			$csl = json_decode($json);
-	
-			$ignore = true;
-	
-			if (isset($csl->unstructured))
-			{
-				$ignore = false;
-				if ($csl->unstructured == '')
-				{
-					$ignore = true;
-				}
-			}
+			$string = $obj->references[$i]->string;
+		
+			$url = $config['parser_url'] . '/parse?string=' . urlencode($string);
 
-			if (!$ignore)
-			{					
+			$json = get($url);
+			if ($json != '')
+			{
+				$csl = json_decode($json);
 	
-				// clean
-				if (isset($csl->matched))
-				{
-					unset($csl->matched);
-				}
-				if (isset($csl->parts))
-				{
-					unset($csl->parts);
-				}
+				$ignore = true;
 	
-				$obj->references[$i]->csl = $csl;
+				if (isset($csl->unstructured))
+				{
+					$ignore = false;
+					if ($csl->unstructured == '')
+					{
+						$ignore = true;
+					}
+				}
+
+				if (!$ignore)
+				{					
+	
+					// clean
+					if (isset($csl->matched))
+					{
+						unset($csl->matched);
+					}
+					if (isset($csl->parts))
+					{
+						unset($csl->parts);
+					}
+	
+					$obj->references[$i]->csl = $csl;
+				}
 			}
 		}
 	}	
